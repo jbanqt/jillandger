9b1bc5be11fd43820617a4d5b487cd52eabdff27
Jocelle <jbanqt@gmail.com>
2025-12-06 00:43:53 +0800

    Fixes

--- gulp/boilerplate/compilers/compilePug.js
+++ gulp/boilerplate/compilers/compilePug.js
@@ -2,10 +2,18 @@
 const gulp = require('gulp');
 const config = require('../config');
 const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const path = require('path');
 
-module.exports = (source, out, pugop = {pretty: true}) => {
+module.exports = (source, out, pugop = { pretty: true }) => {
   return gulp.src(source)
     .pipe(pug(pugop))
-    .pipe(gulp.dest(out))
-  ;
-}
+    .pipe(rename(function (filePath) {
+      // If the file is not index.pug, move it into a folder and rename to index.html
+      if (filePath.basename !== 'index') {
+        filePath.dirname = path.join(filePath.dirname, filePath.basename);
+        filePath.basename = 'index';
+      }
+    }))
+    .pipe(gulp.dest(out));
+};
